932c9272d502386489068a3798ebc45d57d87f73
Zoltan Rajnai <zoltan.rajnai@shapr3d.com>
2024-03-01 14:28:33 +0100

    Refactoring IEnumerable_Tasks ddmo

--- 3_2_IEnumerable_Tasks/Program.cs
+++ 3_2_IEnumerable_Tasks/Program.cs
@@ -3,53 +3,25 @@
 using var input = new FileStream("input.txt", FileMode.Open);
 using var output = new FileStream("output.txt", FileMode.Create);
 
-Console.WriteLine("Main thread Starting...");
+Console.WriteLine($"Main thread Starting... {Thread.CurrentThread.ManagedThreadId}");
 
 var task = CopyStreamToStreamAsync(input, output);
 
-Console.WriteLine("Main thread working...");
+Console.WriteLine($"Main thread working... {Thread.CurrentThread.ManagedThreadId}");
 
 task.Wait();
 
-Console.WriteLine("Main thread exiting.");
+Console.WriteLine($"Main thread exiting. {Thread.CurrentThread.ManagedThreadId}");
+
 #endregion
 
-#region Implementation
-
-static Task IterateAsync(IEnumerable<Task> tasks) {
-    var tcs = new TaskCompletionSource();
-
-    IEnumerator<Task> e = tasks.GetEnumerator();
-
-    void Process() {
-        Console.WriteLine("Background Processing...");
-        try {
-            if (e.MoveNext()) { // If there are more tasks to process
-                e.Current.ContinueWith(_ => Process()); // Set the next tasks continuation to call Process again
-                return;
-            }
-        }
-        catch (Exception e) {
-            tcs.SetException(e);
-            return;
-        }
-        tcs.SetResult();
-        Console.WriteLine("Background Processing done.");
-    }
-
-    Process();
-
-    return tcs.Task;
-}
-
 static Task CopyStreamToStreamAsync(Stream source, Stream destination) {
-    return IterateAsync(Impl(source, destination));
-
-    static IEnumerable<Task> Impl(Stream source, Stream destination) {
+    static IEnumerable<Task> GetTasks(Stream source, Stream destination) {
         var buffer = new byte[50];
         while (true) {
             Task<int> read = source.ReadAsync(buffer, 0, buffer.Length);
             yield return read;
+
             int numRead = read.Result;
             if (numRead <= 0) {
                 break;
@@ -57,8 +29,33 @@ static Task CopyStreamToStreamAsync(Stream source, Stream destination) {
 
             Task write = destination.WriteAsync(buffer, 0, numRead);
             yield return write;
-            write.Wait();
         }
     }
+
+    static Task CombineTasks(IEnumerable<Task> tasks) {
+        var tcs = new TaskCompletionSource(); // produces a task and allows to set its result
+
+        IEnumerator<Task> e = tasks.GetEnumerator();
+
+        void Process() {
+            Console.WriteLine($"Background Processing... {Thread.CurrentThread.ManagedThreadId}");
+            if (e.MoveNext()) {
+                // If there are more tasks to process
+                // Set the next tasks continuation to call Process again
+                e.Current.ContinueWith(_ => Process());
+                return;
+            }
+
+            // If there are no more tasks to process
+            tcs.SetResult();
+            Console.WriteLine($"Background Processing done. {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        Process();
+
+        return tcs.Task;
+    }
+
+    return CombineTasks(GetTasks(source, destination));
+
 }
-#endregion
